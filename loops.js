0e99b4e96b62e26ac68b939beb03149e0eddc662
vibhu mahendru <vibhumahendru@gmail.com>
2018-08-24 15:56:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,24 +1,15 @@
-//function forLoop(array){
-  //for (var i = 0; i<25; i++){
-    //if(i === 1)
-    //{
-    //array.push("I am 1 strange loop.");
-    //}
-    //else {
-    //  array.push("I am '${i}' strange loops.")
-    //}
-//  }
-  //return array
-//}
 function forLoop(array){
-for (i = 0; i < 25; i++) {
-if(i===1){
-array.push(“I am 1 strange loop.”);
-} else {
-array.push(“I am ${i} strange loop.”)
+for (var i = 0; i<25; i++){
+    if(i === 1){
+   
+  array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push("I am '${i}' strange loops.")
+    }
+  }
+  return array
 }
-}
-return array;
-}
+
 
 
